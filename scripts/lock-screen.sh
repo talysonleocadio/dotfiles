0fd70e7e6000e3eb5a39d7841f63a1975c379467
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2018-11-23 13:22:42 -0200

    Leve mudança no script para apenas borrar a tela quando existe mais de um monitor

--- scripts/lock-screen.sh
+++ scripts/lock-screen.sh
@@ -3,5 +3,10 @@ icon=$HOME/.dotfiles/i3/lock.png
 tmpbg=/tmp/screen.png
 scrot /tmp/screen.png
 convert $tmpbg -scale 10% -scale 1000% $tmpbg
-convert $tmpbg $icon -gravity center -composite -matte $tmpbg
+num_of_screens = xrandr -q | rg 'connected' | wc -l
+
+if [ "$num_of_screens" -lt 1 ];then
+  convert $tmpbg $icon -gravity center -composite -matte $tmpbg
+fi
+
 i3lock -u -i $tmpbg
