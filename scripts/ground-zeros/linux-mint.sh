b22fb7e2be0c2f0d73aa0fd28b01f0aca54cc893
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2018-12-26 14:26:17 -0200

    Instalações do kde-connect, i3 com cinnamon e albert

--- scripts/ground-zeros/linux-mint.sh
+++ scripts/ground-zeros/linux-mint.sh
@@ -5,7 +5,7 @@ upgrade="sudo apt upgrade -y"
 install="sudo apt install -y"
 addppa="sudo add-apt-repository"
 
-function printWhereYouAre {
+function log {
     printf "\n\n"
     echo \[$1\]
 }
@@ -14,18 +14,47 @@ function removeFile {
     rm $1
 }
 
-printWhereYouAre "Atualizando os pacotes"
+log "PPA: vscode"
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+log "PPA: kde-connect"
+$addppa ppa:webupd8team/indicator-kdeconnect 
+
+log "Atualizando os pacotes"
 $update
 $upgrade
 
-printWhereYouAre "Instalações básicas"
-$install build-essential curl wget neovim tilda tmux libhidapi-dev rlwrap
+log "Instalações básicas"
+$install build-essential curl wget neovim tilda tmux libhidapi-dev rlwrap emacs code indicator-kdeconnect
 
-printWhereYouAre "ZSH"
+log "i3 (Gaps)"
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+log "i3+cinnamon"
+git clone https://github.com/jbbr/i3-cinnamon.git ~/i3-cinnamon
+cd ~/i3-cinnamon && sudo make install
+cd .. && rm -rf i3-cinnamon
+ 
+log "Albert"
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
+sudo apt-get update
+sudo apt-get install albert
+
+log "ZSH"
 $install zsh
 chsh -s $(which zsh)
 
-printWhereYouAre "OH-MY-ZSH"
+log "OH-MY-ZSH"
 if [ -d "$HOME/.oh-my-zsh" ]; then
     echo "O diretório já existe removendo..."
     rm -rf $HOME/.oh-my-zsh
@@ -33,7 +62,10 @@ fi
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
-printWhereYouAre "OH-MY-ZSH: SPACESHIP"
+log "Spacemacs"
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+log "OH-MY-ZSH: SPACESHIP"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
@@ -44,33 +76,28 @@ $HOME/.oh-my-zsh/custom/themes/spaceship-prompt
 ln -s $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme \
 $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
 
-printWhereYouAre "OH-MY-ZSH: ZSH-SYNTAX-HIGHLIGHT"
+log "OH-MY-ZSH: ZSH-SYNTAX-HIGHLIGHT"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-printWhereYouAre "OH-MY-ZSH: ZSH-AUTOSUGGESTIONS"
+log "OH-MY-ZSH: ZSH-AUTOSUGGESTIONS"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git \
 $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-printWhereYouAre "FZF"
+log "FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 rm $HOME/.zshrc
 
-printWhereYouAre "RIPGREP"
+log "RIPGREP"
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.9.0/ripgrep_0.9.0_amd64.deb
 sudo dpkg -i ripgrep_0.9.0_amd64.deb
 removeFile ripgrep_0.9.0_amd64.deb
 
-printWhereYouAre "VS CODE"
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-$update
-$install code
+log "Remoção das configs default do vscode"
 rm -rf $HOME/.config/Code/User/*.json
 
-printWhereYouAre "Instalações para o G610"
+log "Instalações para o G610"
 git clone https://github.com/MatMoul/g810-led.git ~/.g610-led
 cd ~/.g610-led
 make bin
@@ -78,7 +105,7 @@ sudo make install
 sudo rm /etc/g810-led/profile
 sudo cp $HOME/.g610-led/profile /etc/g810-led
 
-printWhereYouAre "Configurações do Neovim"
+log "Configurações do Neovim"
 mkdir -p ~/.config/nvim/colors
 ln -sf /home/talyson/.dotfiles/nvim/init.vim ~/.config/nvim
 
@@ -89,9 +116,12 @@ mv ~/.config/nvim/colors/molokai/colors/molokai.vim ~/.config/nvim/colors
 rm -rf ~/.config/nvim/colors/molokai
 vim +'PlugInstall' +qa
 
-printWhereYouAre "Criando os symlinks"
+log "Criando os symlinks"
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
 ln -sf ~/.dotfiles/vscode/settings/* ~/.config/Code/User
 ln -sf ~/.dotfiles/ripgrep/.ripgreprc ~/
 sudo ln -sf ~/.dotfiles/g610/profile /etc/g810-led
+ln -sf ~/.dotfiles/spacemacs/.spacemacs ~/
+ln -sf ~/.dotfiles/i3/config ~/.config/i3
+ln -sf ~/.dotfiles/i3status/config ~/.config/i3status
