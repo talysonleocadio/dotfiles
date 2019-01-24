ca9f4932057b3b34c621cf0e40e41d5a9cbee1e0
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-01-24 10:52:00 -0200

    Adição do compton nas instalações base do sistema

--- scripts/ground-zeros/antergos-i3.sh
+++ scripts/ground-zeros/antergos-i3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function printWhereYouAre {
+function log {
     printf "\n\n"
     echo \[$1\]
 }
@@ -12,20 +12,20 @@ function removeFile {
 update="sudo pacman -Syu --noconfirm"
 install="sudo pacman -S --noconfirm"
 
-printWhereYouAre "Atualizando pacotes e instalacoes de pacotes presentes nos repositórios do arch-linux"
-$update && $install neovim yay ripgrep firefox tmux rlwrap hidapi zsh code anki feh gnome-screenshot redshift imagemagick xautolock i3-gaps bluez blueberry emacs
+log "Atualizando pacotes e instalacoes de pacotes presentes nos repositórios do arch-linux"
+$update && $install neovim yay ripgrep firefox tmux rlwrap hidapi zsh code anki feh gnome-screenshot redshift imagemagick xautolock i3-gaps bluez blueberry emacs compton
 
-printWhereYouAre "Instalações AUR (Thanks Community)"
+log "Instalações AUR (Thanks Community)"
 aurinstall="yay -S --aur --noconfirm"
-$aurinstall google-chrome spotify mintstick clipit albert i3-cinnamon mint-y-icons mint-backgrounds-tara nodejs-n
+$aurinstall spotify mintstick clipit albert i3-cinnamon mint-y-icons mint-backgrounds-tara nodejs-n
 
-printWhereYouAre "Instalação do nodejs lts"
+log "Instalação do nodejs lts"
 n lts
 
-printWhereYouAre "Removendo Default configs do Code"
+log "Removendo Default configs do Code"
 rm -rf $HOME/.config/Code\ -\ OSS/User/*.json
 
-printWhereYouAre "Removendo default configs do I3"
+log "Removendo default configs do I3"
 if [ -d "$HOME/.config/i3" ]; then
     echo "O diretório i3 existe, removendo config do i3"
     removeFile $HOME/.config/i3/config
@@ -40,11 +40,11 @@ else
     mkdir -p ~/.config/i3status
 fi
 
-printWhereYouAre "ZSH"
+log "ZSH"
 $install zsh
 chsh -s $(which zsh)
 
-printWhereYouAre "OH-MY-ZSH"
+log "OH-MY-ZSH"
 if [ -d "$HOME/.oh-my-zsh" ]; then
     echo "O diretório já existe removendo..."
     rm -rf $HOME/.oh-my-zsh
@@ -52,7 +52,7 @@ fi
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
-printWhereYouAre "OH-MY-ZSH: SPACESHIP"
+log "OH-MY-ZSH: SPACESHIP"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
@@ -63,20 +63,20 @@ $HOME/.oh-my-zsh/custom/themes/spaceship-prompt
 ln -s $HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme \
 $HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme
 
-printWhereYouAre "OH-MY-ZSH: ZSH-SYNTAX-HIGHLIGHT"
+log "OH-MY-ZSH: ZSH-SYNTAX-HIGHLIGHT"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
 $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-printWhereYouAre "OH-MY-ZSH: ZSH-AUTOSUGGESTIONS"
+log "OH-MY-ZSH: ZSH-AUTOSUGGESTIONS"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git \
 $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-printWhereYouAre "FZF"
+log "FZF"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 removeFile $HOME/.zshrc
 
-printWhereYouAre "Configurações do Neovim"
+log "Configurações do Neovim"
 mkdir -p ~/.config/nvim/colors
 git clone https://github.com/sickill/vim-monokai.git monokai
 mv monokai/colors/monokai.vim ~/.config/nvim/colors
@@ -90,10 +90,10 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 nvim +'PlugInstall' +qa
 
-printWhereYouAre "Pastas de configuração para termite e redshift"
+log "Pastas de configuração para termite e redshift"
 mkdir -p ~/.config/{redshift,termite}
 
-printWhereYouAre "Criando os symlinks"
+log "Criando os symlinks"
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
 ln -sf ~/.dotfiles/vscode/settings/* ~/.config/Code\ -\ OSS/User
