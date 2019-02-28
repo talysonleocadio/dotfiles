77f4e6dd1a4b9fc5c66e3ad2cbbbaa4c6bbf8b28
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-02-28 15:59:28 -0300

    Instalação necessária para ts

--- scripts/system-install/neovim.sh
+++ scripts/system-install/neovim.sh
@@ -3,6 +3,7 @@
 log "NEOVIM: Instalação de dependências"
 yay -S editorconfig-core-c
 pip install --user neovim
+sudo npm i -g neovim
 
 log "NEOVIM: Criação da pasta e symlink da configuração"
 mkdir -p ~/.config/nvim
