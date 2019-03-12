72cf430f4bfa6cb0df0d71e43358d84a5e93fd50
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-03-12 09:54:34 -0300

    Moção dos symlinks para seus respectivos scripts de instalação

--- scripts/system-install/antergos-i3.sh
+++ scripts/system-install/antergos-i3.sh
@@ -9,14 +9,8 @@ update="sudo pacman -Syu --noconfirm"
 install="sudo pacman -S --noconfirm"
 yay="yay -S --aur --noconfirm"
 
-log "Criando os symlinks"
-ln -sf ~/.dotfiles/zsh/.zshrc ~/
-ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
-ln -sf ~/.dotfiles/ripgrep/.ripgreprc ~/
-sudo ln -sf ~/.dotfiles/g610/profile /etc/g810-led
-sudo ln -sf ~/.dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
-ln -sf ~/.dotfiles/i3/config ~/.config/i3
-ln -sf ~/.dotfiles/i3status/config ~/.config/i3status
-ln -sf ~/.dotfiles/albert/albert.conf ~/.config/albert
-cp ~/.dotfiles/redshift/redshift.conf ~/.config/redshift
-ln -sf ~/.dotfiles/termite/config ~/.config/termite
+source $HOME/.dotfiles/scripts/system-install/base-install.sh
+source $HOME/.dotfiles/scripts/system-install/oh-my-zsh.sh
+source $HOME/.dotfiles/scripts/system-install/fzf.sh
+source $HOME/.dotfiles/scripts/system-install/dev-environment.sh
+source $HOME/.dotfiles/scripts/system-install/neovim.sh
--- scripts/system-install/base-install.sh
+++ scripts/system-install/base-install.sh
@@ -12,3 +12,12 @@ $yay spotify mintstick clipit albert g810-led-git
 log "Criação de pastas para receber as confs do termite e redshift"
 mkdir -p ~/.config/{redshift,termite};
 
+log "Symlinks das instalações base do sistema"
+ln -sf ~/.dotfiles/termite/config ~/.config/termite
+ln -sf ~/.dotfiles/albert/albert.conf ~/.config/albert
+ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
+ln -sf ~/.dotfiles/zsh/.zshrc ~/
+ln -sf ~/.dotfiles/ripgrep/.ripgreprc ~/
+sudo rm /etc/g810-led/profile && \
+  cp ~/.dotfiles/g610/profile /etc/g810-led
+cp ~/.dotfiles/redshift/redshift.conf ~/.config/redshift
