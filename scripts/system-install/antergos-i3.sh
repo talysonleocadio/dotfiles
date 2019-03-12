e83f6279fb31ea8027c517fbfd34b1a25a0c6f41
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-03-12 08:28:54 -0300

    Moção de código para o seu respectivo script

--- scripts/system-install/antergos-i3.sh
+++ scripts/system-install/antergos-i3.sh
@@ -7,20 +7,7 @@ function log {
 
 update="sudo pacman -Syu --noconfirm"
 install="sudo pacman -S --noconfirm"
-
-log "Otimização dos mirrors"
-$install reflector
-sudo reflector --protocol https --sort score --latest 5 --save /etc/pacman.d/mirrorlist
-
-log "Atualizando pacotes e instalacoes de pacotes presentes nos repositórios do arch-linux"
-$update && $install yay ripgrep firefox tmux rlwrap hidapi zsh anki redshift xclip 
-
-log "Instalações AUR"
 yay="yay -S --aur --noconfirm"
-$yay spotify mintstick clipit albert g810-led-git
-
-log "Pastas de configuração para termite e redshift"
-mkdir -p ~/.config/{redshift,termite};
 
 log "Criando os symlinks"
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
