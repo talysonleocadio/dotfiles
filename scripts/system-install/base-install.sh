1e9946781f5b7207fbb6671830df7fbc7220d5ee
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-03-19 11:42:38 -0300

    Melhorias no processo de instalação dos programas base do sistema

--- scripts/system-install/base-install.sh
+++ scripts/system-install/base-install.sh
@@ -3,21 +3,21 @@ log "Otimização dos mirrors"
 $install reflector
 sudo reflector --protocol https --sort score --latest 5 --save /etc/pacman.d/mirrorlist
 
-log "Update e instalação de programas dos repositórios oficiais"
-$update && $install yay ripgrep firefox tmux rlwrap hidapi zsh anki redshift xclip
+log "Instalação de programas base"
+$update && $install yay ripgrep firefox tmux rlwrap hidapi zsh anki redshift xclip chromium albert
 
-log "Instalações AUR"
-$yay spotify mintstick clipit albert g810-led-git
+log "Instalação de programas presentes no AUR"
+$yay spotify mintstick g810-led-git
 
-log "Criação de pastas para receber as confs do termite e redshift"
+log "Criação de pastas para as confs do termite e redshift"
 mkdir -p ~/.config/{redshift,termite};
 
-log "Symlinks das instalações base do sistema"
+log "Symlinks dos programas base do sistema"
 ln -sf ~/.dotfiles/termite/config ~/.config/termite
 ln -sf ~/.dotfiles/albert/albert.conf ~/.config/albert
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
 ln -sf ~/.dotfiles/ripgrep/.ripgreprc ~/
 sudo rm /etc/g810-led/profile && \
-  cp ~/.dotfiles/g610/profile /etc/g810-led
+  sudo cp ~/.dotfiles/g610/profile /etc/g810-led
 cp ~/.dotfiles/redshift/redshift.conf ~/.config/redshift
