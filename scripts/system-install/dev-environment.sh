e6e4f5f01d9342a5520ff1e7b347c431f0e7e285
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2019-03-29 17:21:52 -0300

    Yarn para o coc.nvim

--- scripts/system-install/dev-environment.sh
+++ scripts/system-install/dev-environment.sh
@@ -9,5 +9,5 @@ pip install --user flake8 neovim pipenv vim-vint
 log "NODE: Instalação da última versão LTS"
 sudo n lts
 
-log "JS: Instalação do Eslint, TS e tslint"
-sudo npm i -g eslint typescript tslint
+log "JS: Instalação do Eslint, TS e tslint e yarn"
+sudo npm i -g eslint typescript tslint yarn
