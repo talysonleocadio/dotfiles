18088770b2fd52da206978043463ff894b1144fe
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-03-27 19:28:17 -0300

    Renome de arquivo.
    A instalação não diz mais respeito ao saudoso antergos e sim ao Arch
    Linux vanilla.

--- scripts/system-install/arch-linux-install.sh
+++ scripts/system-install/arch-linux-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 function log {
   printf "\n\n"
@@ -6,8 +6,8 @@ function log {
 }
 
 update="sudo pacman -Syu --noconfirm"
-install="sudo pacman -S --noconfirm"
-yay="yay -S --aur --noconfirm"
+install="sudo pacman -S --noconfirm --needed"
+yay="yay -S --aur --noconfirm --needed"
 
 source $HOME/.dotfiles/scripts/system-install/base-install.sh
 source $HOME/.dotfiles/scripts/system-install/oh-my-zsh.sh
