d3769c7c9d6297baea6f515cfa6bca83de2da3eb
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-03-27 19:29:26 -0300

    Mudança na instalação do i3.
    Antes, a solução era a instalação do cinnamon usando o i3 como
    gerenciador de janelas. Agora, o plasma será usado no lugar.

--- scripts/system-install/i3.sh
+++ scripts/system-install/i3.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 log "Instalações necessárias para o i3"
-$yay cinnamom i3-gaps i3-cinnamon mint-y-icons mint-backgrounds-tara spectacle xcompton xautolock
+$yay i3-gaps xcompton xautolock
+
+log "Instalações para o KDE plasma"
+$yay sddm plasma-desktop konsole dolphin discover kate spectacle
+systemctl enable sddm
 
 log "Symlinks dos arquivos de configuração"
 ln -sf ~/.dotfiles/i3/config ~/.config/i3
 ln -sf ~/.dotfiles/i3status/config ~/.config/i3status
 sudo ln -sf ~/.dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
+sudo ln -sf ~/.dotfiles/xorg/plasma-i3.desktop /usr/share/xsessions
