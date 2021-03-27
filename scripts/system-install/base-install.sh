cc766a1465f45b0a32b2df1b42f2a8592c82f139
Talyson Leocadio <talyson.rafael.leocadio@gmail.com>
2021-03-27 19:32:55 -0300

    Mudança na instalação de alguns pacotes base.
    A forma como o yay é instalado muda do antergos em relação ao Arch
    vanilla, também removi o termite da instalação base do sistema.

--- scripts/system-install/base-install.sh
+++ scripts/system-install/base-install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 log "Otimização dos mirrors"
-$install reflector
+$update && $install reflector
 sudo reflector --protocol https --sort score --latest 5 --save /etc/pacman.d/mirrorlist
 
 log "Instalação de programas base"
-$update && $install yay ripgrep firefox tmux rlwrap hidapi zsh anki redshift xclip chromium bat
+$install base-devel ripgrep firefox tmux rlwrap hidapi zsh anki redshift xclip chromium bat git
+
+log "Instalação do yay"
+git clone https://aur.archlinux.org/yay.git
+cd yay && makepkg -si
+cd .. && rm -rf yay
 
 log "Instalação de programas presentes no AUR"
-$yay spotify mintstick g810-led-git
+$yay g810-led-git nerd-fonts-hack albert
 
-log "Criação de pastas para as confs do termite e redshift"
-mkdir -p ~/.config/{redshift,termite};
+log "Criação de pastas para as confs do redshift"
+mkdir -p ~/.config/{redshift};
 
 log "Symlinks dos programas base do sistema"
-ln -sf ~/.dotfiles/termite/config ~/.config/termite
 ln -sf ~/.dotfiles/albert/albert.conf ~/.config/albert
 ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
