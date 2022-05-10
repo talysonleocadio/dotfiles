d8f8309c059580b8c494013b8b02ba3ce83c4406
Talyson Leocádio <talyson.rafael.leocadio@gmail.com>
2022-05-10 10:34:40 -0300

    Ajustes finais no instalador.

--- scripts/system-install/base-install.sh
+++ scripts/system-install/base-install.sh
@@ -11,8 +11,8 @@ cd .. && rm -rf yay
 logs "Instalação de programas presentes no AUR"
 $yay g810-led-git nerd-fonts-hack albert
 
-logs "Criação de pastas para as confs do redshift"
-mkdir -p ~/.config/{redshift};
+log "Criação de pastas para as confs do redshift"
+mkdir -p ~/.config/{redshift,albert,i3,i3status}
 
 logs "Symlinks dos programas base do sistema"
 ln -sf ~/.dotfiles/albert/albert.conf ~/.config/albert
@@ -20,5 +20,10 @@ ln -sf ~/.dotfiles/tmux/.tmux.conf ~/
 ln -sf ~/.dotfiles/zsh/.zshrc ~/
 ln -sf ~/.dotfiles/ripgrep/.ripgreprc ~/
 ln -sf ~/.dotfiles/git/.gitconfig ~/
-sudo cp ~/.dotfiles/g610/profile /etc/g810-led
+ln -sf ~/.dotfiles/i3/config ~/.config/i3
+ln -sf ~/.dotfiles/i3status/config ~/.config/i3status
 cp ~/.dotfiles/redshift/redshift.conf ~/.config/redshift
+
+sudo ln -sf ~/.dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
+sudo cp ~/.dotfiles/xorg/plasma-i3.desktop /usr/share/xsessions
+sudo cp ~/.dotfiles/g610/profile /etc/g810-led
--- scripts/system-install/i3.sh
+++ scripts/system-install/i3.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 log "Instalações necessárias para o i3"
-$yay i3-gaps xcompton xautolock
+$yay i3-gaps picom xautolock feh
 
 log "Instalações para o KDE plasma"
 $yay sddm plasma-desktop konsole dolphin discover kate spectacle
 systemctl enable sddm
-
-log "Symlinks dos arquivos de configuração"
-ln -sf ~/.dotfiles/i3/config ~/.config/i3
-ln -sf ~/.dotfiles/i3status/config ~/.config/i3status
-sudo ln -sf ~/.dotfiles/xorg/30-touchpad.conf /etc/X11/xorg.conf.d
-sudo ln -sf ~/.dotfiles/xorg/plasma-i3.desktop /usr/share/xsessions
